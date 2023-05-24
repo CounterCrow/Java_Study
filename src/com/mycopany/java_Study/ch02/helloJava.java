8f79c20308cda7cb5b054a8df9509bae88ebed42
countercrow <hongoppa@naver.com>
2023-05-24 16:18:51 +0900

    자바 처음으로 커밋

---
+++ src/com/mycopany/java_Study/ch02/helloJava.java
@@ -0,0 +1,10 @@
+package com.mycopany.java_Study.ch02;
+
+public class helloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("helloWord");
+	}
+
+}
