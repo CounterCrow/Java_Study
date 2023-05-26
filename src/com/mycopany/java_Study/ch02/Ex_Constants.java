c510f77c3b4f0fdfaac0b7eecbceebcc653dba09
countercrow <hongoppa@naver.com>
2023-05-26 16:31:45 +0900

    java 상수 업데이트

---
+++ src/com/mycopany/java_Study/ch02/Ex_Constants.java
@@ -0,0 +1,16 @@
+package com.mycopany.java_Study.ch02;
+
+public class Ex_Constants {
+
+	public static final String MY_NAME = "홍영기";
+	public static final Double PAI = 3.14;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		public static final String MY_NAME = "홍영기";	
+		System.out.println("MY_NAME: " + MY_NAME);
+//		MY_NAME = "countercrow";		//error		
+	}
+
+}
