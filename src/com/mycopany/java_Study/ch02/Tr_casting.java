39b9a5a1380d05e72c70777517e4e7172b985cde
countercrow <hongoppa@naver.com>
2023-05-26 17:40:35 +0900

    캐스팅 복습

---
+++ src/com/mycopany/java_Study/ch02/Tr_casting.java
@@ -0,0 +1,31 @@
+package com.mycopany.java_Study.ch02;
+
+public class Tr_casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	double>float>long>int>short>byte
+	double doubleVar = 123456789;
+	
+	float floatVar = (float)doubleVar;
+	System.out.println("floatVar: "+floatVar);
+	long longVar = (long)floatVar;
+	System.out.println("longVar: "+longVar);
+	int intVar = (int)longVar;
+	System.out.println("intVar: "+intVar);
+	short shortVar = (short)intVar;
+	System.out.println("shortVar: "+shortVar);
+	byte byteVar = (byte)intVar;
+	System.out.println("byteVar: "+byteVar);
+	
+	System.out.println("---------------------------");
+	
+	String stringVar = String.valueOf(intVar);
+	System.out.println("stringVar: "+stringVar);
+	
+	int intVar2 = Integer.parseInt(stringVar);
+	System.out.println("intVar2: "+intVar2);
+		
+	}
+
+}
