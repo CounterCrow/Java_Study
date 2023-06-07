1498002a94158b8bb6cc29e9078bcc080f3a931c
countercrow <hongoppa@naver.com>
2023-06-07 17:31:16 +0900

    .모델링 + 로또번호생성기

--- src/com/mycopany/java_Study/ch05/ExRandom.java
+++ src/com/mycopany/java_Study/ch05/ExRandom.java
@@ -7,6 +7,14 @@ public class ExRandom {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int min = 1;
+		int max = 45;
+		int random = (int)((Math.random()*(max-min))+min);
+		System.out.println("random: "+ random);
+		
+		
+		
+		
 		int[] lotto = new int[45];
 		int i = 0;
 		while(i<45) {
